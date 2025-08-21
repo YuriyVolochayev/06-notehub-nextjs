d4b4c72f6eb2428a3bb656ed55bf60f8e4a8779c
volochayev <stingray8888831@gmail.com>
2025-08-21 23:15:43 +0300

    add Header, Footer, components from previous HW, loading and error

---
+++ app/notes/error.tsx
@@ -0,0 +1,12 @@
+"use cleint"
+
+type Props = {
+    error: Error;
+};
+
+const Error = ({ error }: Props) => {
+    return <p>Could not fetch the list of notes. {error.message}</p>
+
+};
+
+export default Error
---
+++ component/NoteList/NoteList.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import css from '@/component/NoteList/NoteList.module.css'
+import type { Note } from '../../types/note'
+import { deleteNote } from '@/lib/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Loading } from "notiflix"
+import toast from 'react-hot-toast'
+
+interface NoteListProps {
+    notes: Note[];
+}
+
+const NoteList = ({ notes }: NoteListProps) => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: deleteNote,
+
+        onSuccess: () => {
+            Loading.remove()
+            toast.success("Note created successfully")
+            queryClient.invalidateQueries({ queryKey: ['notes'] });
+        },
+
+        onError: () => {
+            Loading.remove()
+            toast.error("Error creating note")
+        },
+
+    })
+
+    const onDelete = (id: string) => {
+            Loading.pulse()
+            mutation.mutate(id)
+        }
+
+    if (notes.length === 0) return null;
+
+    return (
+        <ul className={css.list}>
+            {notes.map((note) => (
+                <li className={css.listItem} key={note.id}>
+                    <h2 className={css.title}>{note.title}</h2>
+                    <p className={css.content}>{note.content}</p>
+                    <div className={css.footer}>
+                        <span className={css.tag}>{note.tag}</span>
+                        <button className={css.button} onClick={() => onDelete(note.id)}>Delete</button>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default NoteList
